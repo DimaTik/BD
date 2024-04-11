7f90424a78600a11edad43b26a366459f62d7c64
Dima <dima290012007@yandex.ru>
2024-04-11 21:45:41 +0300

    Modification _add_record

--- frontend.py
+++ frontend.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinterdnd2 as tkdnd
 
 PAD = 5
 
@@ -13,9 +14,9 @@ class MainWindow:
 
 
 class Search:
-	HEADINGS = {'country': 'Страна', 'name': 'Название', 'type': 'Тип', 'paster': 'Пастеризация', 'filter': 'Фильтрация', 'barcode': 'Штрих-код', 'nach': 'Начальный алкоголь', 'alc': 'Алкоголь', 'carb': 'Углеводы', 'prot': 'Белки', 'kcal': 'КилоКаллории', 'kjl': 'КилоДжоули', 'vol': 'Объем', 'ibu': 'IBU', 'ebc': 'EBC', 'manuf': 'Производитель', 'link': 'Ссылка'}
+	HEADINGS = {'country': 'Страна', 'name': 'Название', 'type': 'Тип', 'paster': 'Пастеризация', 'filter': 'Фильтрация', 'barcode': 'Штрих-код', 'nach': 'Начальный алкоголь', 'alc': 'Алкоголь', 'carb': 'Углеводы', 'prot': 'Белки', 'fat': 'Жиры', 'kcal': 'КилоКаллории', 'kjl': 'КилоДжоули', 'vol': 'Объем', 'ibu': 'IBU', 'ebc': 'EBC', 'container': 'Тара', 'manuf': 'Производитель', 'link': 'Ссылка', 'image': 'Скан'}
 	show = ['name', 'type', 'paster']
-	list = ('country', 'name', 'type', 'paster', 'filter', 'barcode', 'nach', 'alc', 'carb', 'prot', 'kcal', 'kjl', 'vol', 'ibu', 'ebc', 'manuf', 'link')
+	list = ('country', 'name', 'type', 'paster', 'filter', 'barcode', 'nach', 'alc', 'carb', 'prot', 'fat', 'kcal', 'kjl', 'vol', 'ibu', 'ebc', 'container', 'manuf', 'link', 'image')
 	country = ('Германия', 'Бельгия', 'Чехия и Словакия', 'Англия', 'Украина', 'СНГ', 'Карибы', 'Прибалтика', 'Европа', 'Азия', 'Африка', 'Америка', 'Северная Америка', 'Россия')
 
 	def __init__(self, main):
@@ -63,20 +64,41 @@ class Search:
 
 	# Потом мейби напишешь штуку выбора строки
 
+	def _dnd_image(self):
+		pass
+
 	def _add_record(self):
-		self.list_entr = map(lambda x: 'entr_' + x, self.list)
-		self.list_txt = map(lambda x: 'txt_' + x, self.list)
-		self.root_rec = tk.Tk()
+		self.list_entr = list(map(lambda x: 'entr_' + x, self.list[:-2]))
+		self.list_txt = list(map(lambda x: 'txt_' + x, self.list[:-2]))
+		self.root_rec = tkdnd.Tk()
+		self.part_len = int(len(self.list_txt)/2)
 		self.root_rec.resizable(False, False)
 		self.root_rec.title('Добавление данных')
-		self.root_rec.geometry('500x570')
-		for txt, entr, text, i in zip(self.list_txt, self.list_entr, self.list, range(len(self.list))):
+		self.root_rec.geometry(f'1000x850')
+		for txt, entr, text, i in zip(self.list_txt[:self.part_len], self.list_entr[:self.part_len], self.list[:self.part_len], range(self.part_len)):
 			self.txt = tk.Label(self.root_rec, text=f'{self.HEADINGS[text]}')
 			self.txt.grid(sticky='w', column=0, row=i, padx=PAD, pady=PAD)
 			self.entr = tk.Entry(self.root_rec, width=50)
 			self.entr.grid(column=1, row=i, padx=PAD, pady=PAD)
+		for txt, entr, text, i in zip(self.list_txt[self.part_len:], self.list_entr[self.part_len:], self.list[self.part_len:], range(self.part_len)):
+			self.txt = tk.Label(self.root_rec, text=f'{self.HEADINGS[text]}')
+			self.txt.grid(sticky='w', column=2, row=i, padx=PAD, pady=PAD)
+			self.entr = tk.Entry(self.root_rec, width=50)
+			self.entr.grid(column=3, row=i, padx=PAD, pady=PAD)
+		self.txt_link = tk.Label(self.root_rec, text='Ссылка')
+		self.txt_link.grid(sticky='w', column=0, padx=PAD, pady=PAD)
+		self.entr_link = tk.Entry(self.root_rec, width=120)
+		self.entr_link.grid(column=1, columnspan=3, row=self.txt_link.grid_info()['row'], padx=PAD, pady=PAD)
+		self.txt_img = tk.Label(self.root_rec, text='Скан')
+		self.txt_img.grid(sticky='w', row=len(self.list)*2, column=0, padx=PAD, pady=PAD)
+		self.frm_img = tk.Frame(self.root_rec)
+		self.frm_img.grid(column=1, columnspan=3, row=len(self.list)*2)
+		self.entr_img = tk.Text(self.frm_img, height=30, width=90)
+		# self.entr_img = tk.Entry(self.frm_img, width=50)
+		self.entr_img.grid(column=1, row=len(self.list)*2, padx=PAD, pady=PAD)
+		# self.entr_img.drop_target_register()
 		self.btn_appl = tk.Button(self.root_rec, text='Подтвердить')
-		self.btn_appl.grid(column=1, row=len(self.list)*2+1, padx=PAD, pady=PAD)
+		self.btn_appl.grid(column=2, row=len(self.list)*2+1, padx=PAD, pady=PAD)
 		# self.btn_canc = tk.Button(self.root_rec, text='Отмена')
 		# self.btn_canc.grid(column=1, row=len(self.list)*2+1, padx=PAD, pady=PAD)
 
